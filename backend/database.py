0fd96648ccfa1917f0fd3c6ed13863c7227879ca
Bhawana kandoi <kandoibhawana27@gmail.com>
2026-07-15 22:17:45 +0530

    Use DATABASE_URL for PostgreSQL connection

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,22 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="recruitment_db",
-    user="bhawanakandoi"
-)
+load_dotenv()
 
-# Automatically commit each SQL statement
-conn.autocommit = True
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
+# import psycopg2
+
+# conn = psycopg2.connect(
+#     host="localhost",
+#     database="recruitment_db",
+#     user="bhawanakandoi"
+# )
+
+# # Automatically commit each SQL statement
+# conn.autocommit = True
+
+# cursor = conn.cursor()
