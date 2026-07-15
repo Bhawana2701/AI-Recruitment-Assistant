1385d8c0d29f11272305e0befe19a45a49d1785c
Bhawana kandoi <kandoibhawana27@gmail.com>
2026-07-15 13:55:30 +0530

    Lazy load Whisper model

--- backend/speech_to_text.py
+++ backend/speech_to_text.py
@@ -1,8 +1,13 @@
 # speech_to_text.py
 import whisper
 
-model = whisper.load_model("base")
+model = None
 
 def transcribe(video_path):
+    global model
+
+    if model is None:
+        model = whisper.load_model("base")
+
     result = model.transcribe(video_path)
     return result["text"]
