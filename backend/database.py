bfb4573606014eb0e33c8e9491dff378a3742f19
Bhawana kandoi <kandoibhawana27@gmail.com>
2026-07-16 19:35:17 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,31 +1,31 @@
-import psycopg2
-
-conn = psycopg2.connect(
-    host="localhost",
-    database="recruitment_db",
-    user="bhawanakandoi"
-)
-
-# Automatically commit each SQL statement
-conn.autocommit = True
-
-cursor = conn.cursor()
-
-# import os
 # import psycopg2
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# if not DATABASE_URL:
-#     raise Exception("DATABASE_URL environment variable is not set.")
 
 # conn = psycopg2.connect(
-#     DATABASE_URL,
-#     sslmode="require"
+#     host="localhost",
+#     database="recruitment_db",
+#     user="bhawanakandoi"
 # )
 
+# # Automatically commit each SQL statement
+# conn.autocommit = True
+
 # cursor = conn.cursor()
+
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable is not set.")
+
+conn = psycopg2.connect(
+    DATABASE_URL,
+    sslmode="require"
+)
+
+cursor = conn.cursor()
 
